e588bc7fb6b5106f556bee88ff9ba5ea177cf57a
Juan Gonon <juanfegonon27@gmail.com>
2025-01-21 15:07:18 +0000

    mappeado y addd persistence

--- src/main/java/com/mycompany/concesionaria/logica/Automovil.java
+++ src/main/java/com/mycompany/concesionaria/logica/Automovil.java
@@ -1,5 +1,6 @@
 package com.mycompany.concesionaria.logica;
 
+import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Automovil {
+public class Automovil implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
